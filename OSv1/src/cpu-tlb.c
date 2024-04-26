676571ed978357391d8ef95b4c0878717d3bfe3c
NguyenVu <nguyenvu04.work@gmail.com>
2024-04-26 14:37:47 +0700

    first commit

---
+++ OSv1/src/cpu-tlb.c
@@ -0,0 +1,229 @@
+/*
+ * Copyright (C) 2024 pdnguyen of the HCMC University of Technology
+ */
+/*
+ * Source Code License Grant: Authors hereby grants to Licensee 
+ * a personal to use and modify the Licensed Source Code for 
+ * the sole purpose of studying during attending the course CO2018.
+ */
+//#ifdef CPU_TLB
+/*
+ * CPU TLB
+ * TLB module cpu/cpu-tlb.c
+ */
+ 
+#include "mm.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int tlb_change_all_page_tables_of(struct pcb_t *proc,  struct memphy_struct * mp)
+{
+  /* TODO update all page table directory info 
+   *      in flush or wipe TLB (if needed)
+   */
+
+  return 0;
+}
+
+int tlb_flush_tlb_of(struct pcb_t *proc, struct memphy_struct * mp)
+{
+  /* TODO flush tlb cached*/
+#ifdef CPUTLB_FIXED_TLBSZ
+  for (uint32_t i = 0; i < proc->tlb->maxsz; i++) {
+    proc->tlb->storage[i] = 0;
+  }
+#endif
+  return 0;
+}
+
+/*tlballoc - CPU TLB-based allocate a region memory
+ *@proc:  Process executing the instruction
+ *@size: allocated size 
+ *@reg_index: memory region ID (used to identify variable in symbole table)
+ */
+int tlballoc(struct pcb_t *proc, uint32_t size, uint32_t reg_index)
+{
+  addr_t addr;
+  /* By default using vmaid = 0 */
+  int rgid = 0;
+  
+  while (rgid < PAGING_MAX_SYMTBL_SZ && proc->mm->symrgtbl[rgid] != NULL) rgid++;
+  if (rgid == PAGING_MAX_SYMTBL_SZ) {
+#ifdef DEBUG
+    printf("WARNING: Out of memory region\n");
+#endif
+    return -1;
+  }
+
+  int val = __alloc(proc, proc->mm->mmap->vm_id, rgid, size, &addr);
+ 
+  if (val != 0) return -1;
+  proc->regs[reg_index] = addr;
+
+#ifdef DEBUG
+  printf("Allocated %d bytes at %d to register %d\n", size, addr, reg_index);
+#endif
+  /* TODO update TLB CACHED frame num of the new allocated page(s)*/
+  /* by using tlb_cache_read()/tlb_cache_write()*/
+  unsigned long rgstart = proc->mm->symrgtbl[rgid]->rg_start;
+  unsigned long rgend = proc->mm->symrgtbl[rgid]->rg_end;
+  unsigned long pgnum = (rgend - rgstart) / PAGING_PAGESZ;
+  
+  for (int i = 0; i < pgnum; i++) {
+    int pgid = PAGING_PGN((rgstart + i * PAGING_PAGESZ));
+    tlb_cache_write(proc->tlb, proc->pid, pgid, proc->mm->pgd[pgid]);
+  }
+  return val;
+}
+
+/*pgfree - CPU TLB-based free a region memory
+ *@proc: Process executing the instruction
+ *@size: allocated size 
+ *@reg_index: memory region ID (used to identify variable in symbole table)
+ */
+int tlbfree_data(struct pcb_t *proc, uint32_t reg_index)
+{
+  addr_t addr = proc->regs[reg_index];
+  int rgid;
+  for (rgid = 0; rgid < PAGING_MAX_SYMTBL_SZ; rgid++) {
+    if (proc->mm->symrgtbl[rgid] != NULL && proc->mm->symrgtbl[rgid]->rg_start == addr) 
+      break;
+  }
+  if (rgid == PAGING_MAX_SYMTBL_SZ) return -1;
+  __free(proc, proc->mm->mmap->vm_id, rgid);
+  proc->mm->symrgtbl[rgid] = NULL;
+#ifdef DEBUG
+  printf("Freed region %d in register %d\n", addr, reg_index);
+#endif
+  /* TODO update TLB CACHED frame num of freed page(s)*/
+  /* by using tlb_cache_read()/tlb_cache_write()*/
+
+  return 0;
+}
+
+
+/*tlbread - CPU TLB-based read a region memory
+ *@proc: Process executing the instruction
+ *@source: index of source register
+ *@offset: source address = [source] + [offset]
+ *@destination: destination storage
+ */
+int tlbread(struct pcb_t * proc, uint32_t source,
+            uint32_t offset, 	uint32_t destination) 
+{
+  BYTE data;
+	int frmnum = -1;
+  /* TODO retrieve TLB CACHED frame num of accessing page(s)*/
+  /* by using tlb_cache_read()/tlb_cache_write()*/
+  /* frmnum is return value of tlb_cache_read/write value*/
+  addr_t addr = proc->regs[source];
+  int pgn = PAGING_PGN((addr + offset));
+	uint32_t pte;
+  if (tlb_cache_read(proc->tlb, proc->pid, pgn, &pte) == 0) {
+    frmnum = PAGING_FPN(pte);
+  }
+#ifdef IODUMP
+  if (frmnum >= 0)
+    printf("TLB hit at read region=%d offset=%d\n", 
+	         source, offset);
+  else 
+    printf("TLB miss at read region=%d offset=%d\n", 
+	         source, offset);
+#ifdef PAGETBL_DUMP
+  print_pgtbl(proc, 0, -1); //print max TBL
+#endif
+  MEMPHY_dump(proc->mram);
+#endif
+  int val;
+  if (frmnum < 0) {
+    int rgid;
+    for (rgid = 0; rgid < PAGING_MAX_SYMTBL_SZ; rgid++) {
+      if (proc->mm->symrgtbl[rgid] != NULL 
+      && proc->mm->symrgtbl[rgid]->rg_start <= addr 
+      && proc->mm->symrgtbl[rgid]->rg_end > addr) 
+        break;
+    }
+    if (rgid == PAGING_MAX_SYMTBL_SZ) {
+#ifdef DEBUG
+      printf("WARNING: No region found\n");
+#endif
+      return -1;
+    }
+    val = __read(proc, proc->mm->mmap->vm_id, rgid, offset + addr - proc->mm->symrgtbl[rgid]->rg_start, &data);
+  } else {
+    int phyaddr = (frmnum << PAGING_ADDR_FPN_LOBIT) + offset;
+    val = MEMPHY_read(proc->mram, phyaddr, &data);
+  }
+
+  /* TODO update TLB CACHED with frame num of recent accessing page(s)*/
+  /* by using tlb_cache_read()/tlb_cache_write()*/
+  if (val != 0) return -1;
+  proc->regs[destination] = (uint32_t) data;
+#ifdef DEBUG
+  printf("Value read from register %d to register %d: %d\n", source, destination, proc->regs[destination]);
+#endif
+  return 0;
+}
+
+/*tlbwrite - CPU TLB-based write a region memory
+ *@proc: Process executing the instruction
+ *@data: data to be wrttien into memory
+ *@destination: index of destination register
+ *@offset: destination address = [destination] + [offset]
+ */
+int tlbwrite(struct pcb_t * proc, BYTE data,
+             uint32_t destination, uint32_t offset)
+{
+  BYTE val;
+  int frmnum = -1;
+  /* TODO retrieve TLB CACHED frame num of accessing page(s))*/
+  /* by using tlb_cache_read()/tlb_cache_write()
+  frmnum is return value of tlb_cache_read/write value*/
+  addr_t addr = proc->regs[destination];
+  int pgn = PAGING_PGN((addr + offset));
+	uint32_t pte;
+  if (tlb_cache_read(proc->tlb, proc->pid, pgn, &pte) == 0) {
+    frmnum = PAGING_FPN(pte);
+  }
+#ifdef IODUMP
+  if (frmnum >= 0)
+    printf("TLB hit at write region=%d offset=%d value=%d\n",
+	          destination, offset, data);
+	else
+    printf("TLB miss at write region=%d offset=%d value=%d\n",
+            destination, offset, data);
+#ifdef PAGETBL_DUMP
+  print_pgtbl(proc, 0, -1); //print max TBL
+#endif
+  MEMPHY_dump(proc->mram);
+#endif
+  if (frmnum < 0) {
+    int rgid;
+    for (rgid = 0; rgid < PAGING_MAX_SYMTBL_SZ; rgid++) {
+      if (proc->mm->symrgtbl[rgid] != NULL 
+      && proc->mm->symrgtbl[rgid]->rg_start <= addr 
+      && proc->mm->symrgtbl[rgid]->rg_end > addr) 
+        break;
+    }
+    if (rgid == PAGING_MAX_SYMTBL_SZ) {
+#ifdef DEBUG
+      printf("WARNING: No region found\n");
+#endif
+      return -1;
+    }
+    addr = offset + addr - proc->mm->symrgtbl[rgid]->rg_start;
+    val = __write(proc, proc->mm->mmap->vm_id, rgid, addr, data);
+  } else {
+    addr = (frmnum << PAGING_ADDR_FPN_LOBIT) + offset;
+    val = MEMPHY_write(proc->mram, addr, data);
+  }
+
+  /* TODO update TLB CACHED with frame num of recent accessing page(s)*/
+  /* by using tlb_cache_read()/tlb_cache_write()*/
+#ifdef DEBUG
+  printf("Address written from register %d to %d: %d\n", destination, addr, proc->regs[destination]);
+#endif
+  return val;
+}
+
+//#endif
---
+++ OSv1/src/cpu-tlbcache.c
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2024 pdnguyen of the HCMC University of Technology
+ */
+/*
+ * Source Code License Grant: Authors hereby grants to Licensee 
+ * a personal to use and modify the Licensed Source Code for 
+ * the sole purpose of studying during attending the course CO2018.
+ */
+//#ifdef MM_TLB
+/*
+ * Memory physical based TLB Cache
+ * TLB cache module tlb/tlbcache.c
+ *
+ * TLB cache is physically memory phy
+ * supports random access 
+ * and runs at high speed
+ */
+
+
+#include "mm.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define init_tlbcache(mp,sz,...) init_memphy(mp, sz, (1, ##__VA_ARGS__))
+
+/*
+ *  tlb_cache_read read TLB cache device
+ *  @mp: memphy struct
+ *  @pid: process id
+ *  @pgnum: page number
+ *  @value: obtained value
+ */
+int tlb_cache_read(struct memphy_struct * mp, int pid, int pgnum, uint32_t *value)
+{
+   /* TODO: the identify info is mapped to 
+    *      cache line by employing:
+    *      direct mapped, associated mapping etc.
+    */
+   uint32_t* storage = (uint32_t*) mp->storage;
+   int id = (pgnum % (mp->maxsz / 3)) * 3;
+   if (storage[id] != pgnum || storage[id + 1] != pid) return -1;
+   *value = storage[id + 2];
+   #ifdef DEBUG
+   printf("TLB read at %d for page %d\n", id, pgnum);
+   #endif
+   return 0;
+}
+
+/*
+ *  tlb_cache_write write TLB cache device
+ *  @mp: memphy struct
+ *  @pid: process id
+ *  @pgnum: page number
+ *  @value: obtained value
+ */
+int tlb_cache_write(struct memphy_struct *mp, int pid, int pgnum, uint32_t value)
+{
+   /* TODO: the identify info is mapped to 
+    *      cache line by employing:
+    *      direct mapped, associated mapping etc.
+    */
+   uint32_t* storage = (uint32_t*) mp->storage;
+   
+   int id = (pgnum % (mp->maxsz / 3)) * 3;
+   
+   storage[id] = pgnum;
+   storage[id + 1] = pid;
+   storage[id + 2] = value;
+#ifdef DEBUG
+   printf("TLB updated at %d for page %d\n", id, pgnum);
+#endif
+   return 0;
+}
+
+/*
+ *  TLBMEMPHY_read natively supports MEMPHY device interfaces
+ *  @mp: memphy struct
+ *  @addr: address
+ *  @value: obtained value
+ */
+int TLBMEMPHY_read(struct memphy_struct * mp, int addr, BYTE *value)
+{
+   if (mp == NULL)
+     return -1;
+
+   /* TLB cached is random access by native */
+   *value = mp->storage[addr];
+
+   return 0;
+}
+
+
+/*
+ *  TLBMEMPHY_write natively supports MEMPHY device interfaces
+ *  @mp: memphy struct
+ *  @addr: address
+ *  @data: written data
+ */
+int TLBMEMPHY_write(struct memphy_struct * mp, int addr, BYTE data)
+{
+   if (mp == NULL)
+     return -1;
+
+   /* TLB cached is random access by native */
+   mp->storage[addr] = data;
+
+   return 0;
+}
+
+/*
+ *  TLBMEMPHY_format natively supports MEMPHY device interfaces
+ *  @mp: memphy struct
+ */
+
+
+int TLBMEMPHY_dump(struct memphy_struct * mp)
+{
+   /*TODO dump memphy contnt mp->storage 
+    *     for tracing the memory content
+    */
+
+   return 0;
+}
+
+
+/*
+ *  Init TLBMEMPHY struct
+ */
+int init_tlbmemphy(struct memphy_struct *mp, int max_size)
+{
+   mp->storage = (BYTE *)malloc(max_size*sizeof(BYTE));
+   mp->maxsz = max_size;
+
+   mp->rdmflg = 1;
+
+   return 0;
+}
+
+//#endif
